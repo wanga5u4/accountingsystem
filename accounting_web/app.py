e0bdd70f814b1464afdd02a3728b602d6b543025
wangboyan <xingyc33@gmail.com>
2026-06-24 23:10:57 +0900

    web 开始

--- accounting_web/app.py
+++ accounting_web/app.py
@@ -2,5 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return "<h1>记账系统<h1>"
+    return """ <h1>我的记账系统</h1>
+    <p>欢迎使用Boyan的记账系统</p>"""
 app.run(debug=True)
