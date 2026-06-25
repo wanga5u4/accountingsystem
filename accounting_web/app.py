23595bea4c78ccfa424c8265ce5190cb97dffe6c
wangboyan <xingyc33@gmail.com>
2026-06-25 19:56:30 +0900

    add网页

--- accounting_web/app.py
+++ accounting_web/app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask,render_template,request
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return """ <h1>我的记账系统</h1>
-    <p>欢迎使用Boyan的记账系统</p>"""
+    username="boyan"
+    return render_template(
+        "home.html",
+        username=username)
+
+
+@app.route("/add",methods=["GET","POST"])
+def add():
+    if request.method=="GET":
+        return render_template("add.html")
+    else:
+        money=request.form["money"]
+        note=request.form["note"]
+        print(money)
+        print(note)
+        return render_template("add.html")
+
+@app.route("/show")
+def show():
+    return render_template("show.html")
+
+@app.route("/sort")
+def sort():
+    return render_template("sort.html")
+  
+
 app.run(debug=True)
