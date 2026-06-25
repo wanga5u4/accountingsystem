f52328665da035cc150e0a9616fb38f06cd2d335
wangboyan <xingyc33@gmail.com>
2026-06-25 22:39:24 +0900

    show添加

--- accounting_web/app.py
+++ accounting_web/app.py
@@ -1,5 +1,24 @@
 from flask import Flask,render_template,request
+import pandas as pd
 app=Flask(__name__)
+records=[]
+
+def load_data():
+    global records
+    try:
+        df=pd.read_csv("money.csv")
+        records=df.to_dict("records")
+        print("加载成功")
+    except FileNotFoundError:
+        records = []
+        print("没有找到历史记录")
+
+load_data()
+
+def save_data():
+    df=pd.DataFrame(records)
+    df.to_csv("money.csv",index=False)
+
 @app.route("/")
 def home():
     username="boyan"
@@ -13,15 +32,20 @@ def add():
     if request.method=="GET":
         return render_template("add.html")
     else:
-        money=request.form["money"]
+        money=int(request.form["money"])
         note=request.form["note"]
-        print(money)
-        print(note)
+        records.append({
+            "金额":money,
+            "备注":note
+            })
+        save_data()
         return render_template("add.html")
 
 @app.route("/show")
 def show():
-    return render_template("show.html")
+    return render_template(
+    "show.html",
+    records=records)
 
 @app.route("/sort")
 def sort():
